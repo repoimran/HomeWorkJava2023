661d9d5059276ee4f51a91879fd2b694a3ff804b
Imran Ahmed <imsdhk@gmail.com>
2023-02-01 01:43:07 -0500

    HW_J06_02

---
+++ hw6IfElseCondition02/Temperature.java
@@ -0,0 +1,14 @@
+package hw6IfElseCondition02;
+import java.util.Scanner;
+public class Temperature {
+	public static void main(String[] args) {
+		System.out.println("Today's Weather");
+		Scanner sc = new Scanner(System.in);
+		int todaysTemperature = sc.nextInt();
+		if(todaysTemperature < 32) System.out.println("Freezing");
+		else if(todaysTemperature < 55) System.out.println("Pleasant");
+		else if(todaysTemperature < 73) System.out.println("Getting Warmer");
+		else if(todaysTemperature > 101) System.out.println("Very Hot");
+		else System.out.println("Please put a valid temperature");
+	}
+}
