4d298b0db6f376fee3715f63cd892a7ca1299fea
Imran Ahmed <imsdhk@gmail.com>
2023-02-01 01:23:45 -0500

    HW_J05_2

---
+++ hw5Q3Constructor/StudentTest.java
@@ -0,0 +1,12 @@
+package hw5Q3Constructor;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student st = new Student("any name", 107059, 'M', 3.45f, true ); 
+
+	}
+	
+	//Student Name: <your name>, ID: <your id>, Sex: <your sex in char>, Grade: <your grade> and Java Programmer? Ans: true.(expected)
+	//Student Name: any name, ID: 107059, Sex: M, Grade: 3.45 and Java Programmer? Ans: true. (actual)
+}
