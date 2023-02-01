85a85b03cec3f5ee9aeaa7631f8c405d47ad0a06
Imran Ahmed <imsdhk@gmail.com>
2023-02-01 01:32:25 -0500

    HW_J06_01

---
+++ hw6IfElseCondition01/Voter.java
@@ -0,0 +1,10 @@
+package hw6IfElseCondition01;
+public class Voter {
+	public static void main(String[] args) {
+	int age = 35;
+	if (age == 18) System.out.println("I am a Voter");
+	else if (age < 18) System.out.println("I am not a Voter");
+	else if (age > 18) System.out.println("I am a Voter from age 18");
+	else System.out.println("Please add a valid age");
+	}
+}
