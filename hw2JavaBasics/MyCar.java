70465fee474b3e69e6a69cfc98ef928a6022a4b0
Imran Ahmed <imsdhk@gmail.com>
2023-02-01 21:21:16 -0500

    fixed the missing quotations

--- hw2JavaBasics/MyCar.java
+++ hw2JavaBasics/MyCar.java
@@ -1,7 +1,7 @@
 package hw2JavaBasics;
 public class MyCar {
 	public static void main(String[] args) {
-		System.out.print("My name is" + " imran" + ", and I love to drive, My car info is below:");
+		System.out.print("My name is" + " \"imran\"" + ", and I love to drive, My car info is below:");
 		System.out.print("\nColor:\tblack");
 		System.out.print("\nModel:\tany");
 		System.out.print("\nYear:\t2000");
