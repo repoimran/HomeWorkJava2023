ce482d25cb2122f3bcd2a49afbc420fbc0522c79
Imran Ahmed <imsdhk@gmail.com>
2023-02-03 01:01:26 -0500

    finished first part

--- src/hw9Encapsulation/EmployeeTest.java
+++ src/hw9Encapsulation/EmployeeTest.java
@@ -2,13 +2,13 @@ package hw9Encapsulation;
 
 public class EmployeeTest {
 	public static void main(String[] args) {
-		Employee emp1 = new Employee(); 
+		Employee emp1 = new Employee();
 		emp1.setName("imran");
 		emp1.setAge(35);
 		emp1.setCitizen(true);
 		emp1.setSex('M');
-		
-		System.out.println(String.format("I like the articles %s writes on %d other strings", emp1.getName(), emp1.getAge()));
+		System.out.println(String.format("I am %s, my age is %d, gender is: \'%s\' and am i a usa citizen? Ans: %b",
+				emp1.getName(), emp1.getAge(), emp1.getSex(), emp1.isCitizen()));
 	}
 
 }
